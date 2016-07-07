0208cd44acdc6b1ac43b56625c2aa488db9b9512
David Kellum <dek-oss@gravitext.com>
2016-07-07 15:39:07 -0700

    Preliminary fuzz test (failing)

---
+++ test/test_postgresql_fuzz.rb
@@ -0,0 +1,71 @@
+#!/usr/bin/env ruby
+
+#--
+# Copyright (c) 2016 David Kellum
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you
+# may not use this file except in compliance with the License.  You may
+# obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.  See the License for the specific language governing
+# permissions and limitations under the License.
+#++
+
+require_relative 'setup.rb'
+
+require 'human-ql/query_parser'
+require 'human-ql/postgresql_generator'
+
+class TestPostgresqlFuzz < Minitest::Test
+  TC = HumanQL::QueryParser.new
+  PG = HumanQL::PostgreSQLGenerator.new
+
+  DB = Sequel.connect( "postgres://localhost/human_ql_test" )
+
+  # Assert that the round-trip representation via PG
+  # to_tsquery(generated) oesn't error and is as
+  # expected.
+  def assert_pg_parse( hq )
+    ast = TC.parse( hq )
+    if ast
+      pg = PG.generate( ast )
+      begin
+        rt = DB["select to_tsquery(?) as tsquery", pg].first[:tsquery]
+        refute_nil( rt, hq )
+      rescue Sequel::DatabaseError => e
+        fail( "On query #{hq.inspect} : #{ e.to_s }" )
+      end
+    end
+  end
+
+  # Starting point query
+  GENERIC_Q = 'ape | ( boy -cat dog )'.freeze
+
+  # Characters which are likely to cause trouble
+  RANDOM_C = '((({"xyz" **!: ,^# http:///--0.123.467890-})))'.freeze
+
+  def test_fuzz
+    1000.times do
+      s = rand( GENERIC_Q.length )
+      l = rand( GENERIC_Q.length * 2 )
+      q = GENERIC_Q[s,l]
+      20.times do
+        if rand(3) == 1
+          q[rand(q.length)] = fuzz
+        end
+      end
+      assert_pg_parse( q )
+    end
+  end
+
+  def fuzz
+    RANDOM_C[rand(RANDOM_C.length)]
+  end
+
+
+end if defined?( ::Sequel )

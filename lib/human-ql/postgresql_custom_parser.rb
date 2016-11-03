23188c004b07062ba26e43d91279935f9425f639
David Kellum <dek-oss@gravitext.com>
2016-11-03 16:07:35 -0700

    Use the norm_term mechanism to handle quoted op tokens as well

--- lib/human-ql/postgresql_custom_parser.rb
+++ lib/human-ql/postgresql_custom_parser.rb
@@ -18,11 +18,10 @@ require 'human-ql/query_parser'
 
 module HumanQL
 
-  # Extends the generic QueryParser with extra special character and
-  # token filtering so as to avoid syntax errors in PostgreSQL
-  # to_tsquery() for any known input.  Note that this is still a
-  # parser for the HumanQL query language, not anything implemented in
-  # PostgreSQL.
+  # Extends the generic QueryParser with additional special character
+  # filtering so as to avoid syntax errors in PostgreSQL to_tsquery()
+  # for any known input.  Note that this is still a parser for the
+  # HumanQL query language, not anything implemented in PostgreSQL.
   class PostgreSQLCustomParser < QueryParser
 
     # Construct given options to set via base clase or as specified
@@ -46,25 +45,14 @@ module HumanQL
 
       # Phrase support starts in 9.6
       if ( pg_version <=> [9,6] ) >= 0
-        @term_rejects = /[:*!'<>]/
+        @term_rejects = /[()|&:*!'<>]/
       else
         # Disable quote tokens and reject DQUOTE as token character
         self.lquote = nil
         self.rquote = nil
-        @term_rejects = /[:*!'"<>]/
+        @term_rejects = /[()|&:*!'"<>]/
       end
 
-      # Use by custom #norm_phrase_tokens as a superset of the
-      # #lparen, #rparen token patterns removed by default.  In
-      # PostgreSQL proximity expressions for phrases, the '|' and '&'
-      # still need to be filtered.
-      @phrase_token_rejects = /\A[()|&]\z/.freeze
-    end
-
-    def norm_phrase_tokens( tokens )
-      tokens.
-        reject { |t| @phrase_token_rejects === t }.
-        map { |t| norm_term( t ) }
     end
 
     # Replace term_rejects characters with '_' which is punctuation
